b355043c688354fcc545cfcccc1212b4e017a275
Ciarra Stevenett <ciarra.stevenett@gmail.com>
2020-10-26 09:32:38 -0600

    The scan button works, I just need to set the image path correctly and show what scanner you are connected to

--- Scrapbooker-Base/Form1.Designer.cs
+++ Scrapbooker-Base/Form1.Designer.cs
@@ -32,8 +32,8 @@ namespace Stage_Scrapbooker
         private void InitializeComponent()
         {
             this.panel1 = new System.Windows.Forms.Panel();
-            this.lstListOfScanner = new System.Windows.Forms.ListBox();
             this.lblListOfScanner = new System.Windows.Forms.Label();
+            this.lstListOfScanner = new System.Windows.Forms.ListBox();
             this.btnScan = new System.Windows.Forms.Button();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.panel1.SuspendLayout();
@@ -45,73 +45,68 @@ namespace Stage_Scrapbooker
             this.panel1.Controls.Add(this.btnScan);
             this.panel1.Controls.Add(this.lstListOfScanner);
             this.panel1.Controls.Add(this.lblListOfScanner);
-            this.panel1.Location = new System.Drawing.Point(89, 72);
+            this.panel1.Location = new System.Drawing.Point(13, 13);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(364, 462);
+            this.panel1.Size = new System.Drawing.Size(208, 243);
             this.panel1.TabIndex = 0;
-            this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
-            // 
-            // lstListOfScanner
-            // 
-            this.lstListOfScanner.FormattingEnabled = true;
-            this.lstListOfScanner.ItemHeight = 25;
-            this.lstListOfScanner.Location = new System.Drawing.Point(21, 76);
-            this.lstListOfScanner.Name = "lstListOfScanner";
-            this.lstListOfScanner.Size = new System.Drawing.Size(208, 229);
-            this.lstListOfScanner.TabIndex = 1;
             // 
             // lblListOfScanner
             // 
-            this.lblListOfScanner.AutoSize = true;
-            this.lblListOfScanner.Location = new System.Drawing.Point(16, 24);
+            this.lblListOfScanner.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblListOfScanner.Location = new System.Drawing.Point(3, 12);
             this.lblListOfScanner.Name = "lblListOfScanner";
-            this.lblListOfScanner.Size = new System.Drawing.Size(156, 25);
+            this.lblListOfScanner.Size = new System.Drawing.Size(190, 23);
             this.lblListOfScanner.TabIndex = 0;
-            this.lblListOfScanner.Text = "List of Scanner";
-            this.lblListOfScanner.Click += new System.EventHandler(this.label1_Click);
+            this.lblListOfScanner.Text = "List Of Scanner";
+            this.lblListOfScanner.Click += new System.EventHandler(this.lblListOfScanner_Click);
+            // 
+            // lstListOfScanner
+            // 
+            this.lstListOfScanner.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lstListOfScanner.FormattingEnabled = true;
+            this.lstListOfScanner.ItemHeight = 16;
+            this.lstListOfScanner.Location = new System.Drawing.Point(6, 39);
+            this.lstListOfScanner.Name = "lstListOfScanner";
+            this.lstListOfScanner.Size = new System.Drawing.Size(187, 84);
+            this.lstListOfScanner.TabIndex = 1;
             // 
             // btnScan
             // 
-            this.btnScan.Location = new System.Drawing.Point(42, 344);
+            this.btnScan.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnScan.Location = new System.Drawing.Point(6, 160);
             this.btnScan.Name = "btnScan";
-            this.btnScan.Size = new System.Drawing.Size(147, 31);
-            this.btnScan.TabIndex = 2;
+            this.btnScan.Size = new System.Drawing.Size(187, 31);
+            this.btnScan.TabIndex = 1;
             this.btnScan.Text = "Scan";
             this.btnScan.UseVisualStyleBackColor = true;
+            this.btnScan.Click += new System.EventHandler(this.btnScan_Click);
             // 
             // pictureBox1
             // 
-            this.pictureBox1.Location = new System.Drawing.Point(531, 72);
+            this.pictureBox1.Location = new System.Drawing.Point(296, 12);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(562, 462);
-            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.Size = new System.Drawing.Size(280, 243);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 2;
             this.pictureBox1.TabStop = false;
-            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+           // this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1240, 764);
+            this.ClientSize = new System.Drawing.Size(603, 296);
             this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.panel1);
+            this.MinimizeBox = false;
             this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
             this.panel1.ResumeLayout(false);
-            this.panel1.PerformLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
 
-        }
-
-        private void label1_Click(object sender, EventArgs e)
-        {
-            throw new NotImplementedException();
-        }
-
-        private void panel1_Paint(object sender, PaintEventArgs e)
-        {
-            //throw new NotImplementedException();
         }
 
         #endregion
@@ -119,7 +114,7 @@ namespace Stage_Scrapbooker
         private System.Windows.Forms.Panel panel1;
         private System.Windows.Forms.Label lblListOfScanner;
         private System.Windows.Forms.ListBox lstListOfScanner;
-        private Button btnScan;
-        private PictureBox pictureBox1;
+        private System.Windows.Forms.Button btnScan;
+        private System.Windows.Forms.PictureBox pictureBox1;
     }
 }
--- Scrapbooker-Base/Form1.cs
+++ Scrapbooker-Base/Form1.cs
@@ -7,21 +7,24 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-
 using WIA;
 using System.Runtime.InteropServices;
 using System.IO;
+using System.Reflection;
+using System.Windows.Data;
+
+
 
 namespace Stage_Scrapbooker
 {
     public partial class Form1 : Form
     {
+
+        DeviceManager deviceManager = new DeviceManager();
         public Form1()
         {
             InitializeComponent();
         }
-
-       
 
         private void lblListOfScanner_Click(object sender, EventArgs e)
         {
@@ -32,15 +35,17 @@ namespace Stage_Scrapbooker
         {
             try
             {
+                //Create a DeviceManager instance
                 var deviceManager = new DeviceManager();
 
-                for (int i = 1; i <= deviceManager.DeviceInfos.Count; i++) // Loop Through the get List Of Devices.
+                for (int i = 0; i <= deviceManager.DeviceInfos.Count; i++) // Loop Through the get List Of Devices.
                 {
                     if (deviceManager.DeviceInfos[i].Type != WiaDeviceType.ScannerDeviceType) // Skip device If it is not a scanner
                     {
                         continue;
                     }
-                    lstListOfScanner.Items.Add(deviceManager.DeviceInfos[i].Properties["Name"].get_Value());
+
+                    lstListOfScanner.Items.Add(deviceManager.DeviceInfos[i].Properties["Name"]); //gets the list of scanners and adds their name to the listbox
                 }
             }
             catch (COMException ex)
@@ -49,10 +54,7 @@ namespace Stage_Scrapbooker
             }
         }
 
-        private void pictureBox1_Click(object sender, EventArgs e)
-        {
 
-        }
 
         private void btnScan_Click(object sender, EventArgs e)
         {
@@ -78,19 +80,23 @@ namespace Stage_Scrapbooker
 
                 var ScanerItem = device.Items[1]; // select the scanner.
 
-                var imgFile = (ImageFile)ScanerItem.Transfer(FormatID.wiaFormatJPEG); //Retrive an image in Jpg format and store it into a variable.
+                ImageFile imgFile = (ImageFile)ScanerItem.Transfer(FormatID.wiaFormatJPEG); //Retrive an image in Jpg format and store it into a variable.
 
-                var Path = @"E:\ScanImg.jpg"; // save the image in some path with filename.
+                // 1) Get the string representing the application root directory
+                string path = System.IO.Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
 
-                if (File.Exists(Path))
+                // 2) Create a new object DirectoryInfo using our path
+                DirectoryInfo directory = new DirectoryInfo(path);
+                string
+
+                                // 3) Define the image's folder name within the path
+                                imageFolderPath = path + @"\ImagesFolder-Scrapbooker";
+
+                // 4) Check if the directory exists if not, it will create the images folder
+                if (!Directory.Exists(imageFolderPath))
                 {
-                    File.Delete(Path);
+                    DirectoryInfo di = Directory.CreateDirectory(imageFolderPath);
                 }
-
-                imgFile.SaveFile(Path);
-
-                pictureBox1.ImageLocation = Path;
-
             }
             catch (COMException ex)
             {
